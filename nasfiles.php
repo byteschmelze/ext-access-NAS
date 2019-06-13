ecf7ff5d8df80f097b9aef9e31a75b209e043d14
byteschmelze <39740347+byteschmelze@users.noreply.github.com>
2019-06-13 05:48:54 +0200

    Update nasfiles.php

--- nasfiles.php
+++ nasfiles.php
@@ -1,13 +1,15 @@
 <?php
-	$db = new SQLite3('files.db');
-	$results = $db->query('SELECT REF_ID,CLASS,DETAIL_ID,NAME FROM OBJECTS WHERE (REF_ID IS NULL) AND (DETAIL_ID > 20)');
+	$db = new SQLite3('/srv/http/files.db');
+	// finds any values whose PARENT_IDs start with 64 = root directory:
+	$results = $db->query('SELECT PARENT_ID,CLASS,DETAIL_ID,NAME FROM OBJECTS WHERE PARENT_ID LIKE "64%"');
 	while ($row = $results->fetchArray()) {
-		if (($row[1] == 'container.storageFolder') AND ($row[3] != 'lost+found')) {
+		if ($row[1] == 'container.storageFolder') {
+			// directory name as headline if object is a folder:
 			echo "<h2>".$row[3]."</h2>\n";
 		}
-		elseif ($row[3] != 'lost+found') {
-			// change IP:port below (here 192.168.178.10:8200) to the IP:port of your server
-			echo "<a href=\"http://192.168.178.10:8200/MediaItems/".$row[2].".mpg\">".$row[3]."</a><br />\n";
+		else {
+			// media object, insert your own dynDNS address
+			echo "<a href=\"http://test.dyndns.net:8200/MediaItems/".$row[2].".mpg\">".$row[3]."</a><br />\n";
 		}
 	}
 ?>
